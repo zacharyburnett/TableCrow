d274def74617c41b1ba6571bd62bccdf53fd0344
zacharyburnett <zachary.r.burnett@gmail.com>
2021-11-15 14:13:19 +0000

    Deploying to gh-pages from @ zacharyburnett/TableCrow@1effc032c665dc165ff733dae29d64476fb3d7a6 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0.post11.dev0+799143c',
+    VERSION: '1.4.0.post12.dev0+1effc03',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
