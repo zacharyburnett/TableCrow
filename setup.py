624b70b181a4c09d7f0c77a0919c4b3efda04353
zacharyburnett <zachary.r.burnett@gmail.com>
2021-11-15 09:08:35 -0500

    add sphinx to dependencies

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     extras_require={
         'testing': ['pytest', 'pytest-cov', 'pytest-xdist'],
         'development': ['dunamai', 'flake8', 'isort', 'oitnb'],
-        'documentation': ['dunamai', 'm2r2'],
+        'documentation': ['dunamai', 'm2r2', 'sphinx', 'sphinx-rtd-theme'],
     },
 )
