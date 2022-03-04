cfba96098f7ef49f837df7beca11f299eac93971
Zach <zachary.r.burnett@gmail.com>
2022-03-04 09:20:37 -0500

    update build system (#34)
    * update build system
    
    * update test workflow
    
    * update test workflow
    
    * update test workflow
    
    * update test workflow

--- setup.py
+++ setup.py
@@ -1,41 +1,12 @@
-#!/usr/bin/env python
-from setuptools import config, find_packages, setup
+import warnings
+
+from dunamai import Version
+from setuptools import find_packages, setup
 
 try:
-    from dunamai import Version
-except ImportError:
-    import subprocess
-    import sys
+    __version__ = Version.from_any_vcs().serialize()
+except RuntimeError as error:
+    warnings.warn(f'{error.__class__.__name__} - {error}')
+    __version__ = '0.0.0'
 
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
-    from dunamai import Version
-
-metadata = config.read_configuration('setup.cfg')['metadata']
-
-setup(
-    name=metadata['name'],
-    version=Version.from_any_vcs().serialize(),
-    author=metadata['author'],
-    author_email=metadata['author_email'],
-    description=metadata['description'],
-    long_description=metadata['long_description'],
-    long_description_content_type='text/markdown',
-    url=metadata['url'],
-    packages=find_packages(),
-    python_requires='>=3.8',
-    setup_requires=['dunamai', 'setuptools>=41.2'],
-    install_requires=[
-        'psycopg2-binary',
-        'pyproj',
-        'python-dateutil',
-        'shapely',
-        'spatialite',
-        'sshtunnel',
-        'typepigeon>=1.0.2',
-    ],
-    extras_require={
-        'testing': ['pytest', 'pytest-cov', 'pytest-xdist'],
-        'development': ['dunamai', 'flake8', 'isort', 'oitnb'],
-        'documentation': ['dunamai', 'm2r2', 'sphinx', 'sphinx-rtd-theme'],
-    },
-)
+setup(version=__version__, packages=find_packages(exclude=('tests',)), test_suite='tests')
