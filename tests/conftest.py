ec00e0f9a805e5ff6887d4eb2ac7384e40b40d07
zacharyburnett <zachary.r.burnett@gmail.com>
2022-12-14 15:12:00 -0500

    skip spatial

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,12 +8,9 @@ def pytest_collection_modifyitems(config, items):
         return  # let pytest handle this
 
     skip_spatial = pytest.mark.skip(reason='spatial not selected')
-    skip_sqlite = pytest.mark.skip(reason='sqlite not selected')
     skip_postgres = pytest.mark.skip(reason='postgres not selected')
     for item in items:
         if 'spatial' in item.keywords:
             item.add_marker(skip_spatial)
-        if 'sqlite' in item.keywords:
-            item.add_marker(skip_sqlite)
         if 'postgres' in item.keywords:
             item.add_marker(skip_postgres)
